4dbdbcf82d26414773cc1023e676bf8112fbb87c
skurnal2 <49497542+skurnal2@users.noreply.github.com>
2019-09-12 18:13:26 -0600

    Added New MenuItem for Portfolio Page
    Commented out old Menu and instead using aspMenu and aspMenuItem to create the menu.

--- DVCS Exercise/DVCS_Website/Site.Master.designer.cs
+++ DVCS Exercise/DVCS_Website/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace DVCS_Website {
     public partial class SiteMaster {
         
         /// <summary>
+        /// navigationMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu navigationMenu;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
